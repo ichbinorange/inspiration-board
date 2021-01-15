586ac197846ac1f03089e7f049f40712fc5f3299
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-01-15 15:19:24 -0800

    drafted Card and Board by using hardcoded data for wave1

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,15 +7,34 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
-const Board = () => {
+const Board = (props) => {
+  const cardComponents = CARD_DATA.cards.map((card, i) => {
+    return (
+      <div key={i}>
+        <Card
+          text={card.text}
+          emoji={card.emoji}
+          Emoji={card.Emoji}
+          deleteCardCallback={props.deleteCardCallback}
+        />
+      </div>
+    )
+  })
   return (
-    <div>
-      Board
+    <div className="board">
+      {cardComponents}
     </div>
   )
 };
 Board.propTypes = {
-
+  cards: PropTypes.arrayOf(PropTypes.shape(
+    {
+      quote: PropTypes.string.isRequired,
+      emoji: PropTypes.string,
+      id: PropTypes.number.isRequired,
+    },
+  )),
+  deleteCardCallback: PropTypes.func.isRequired,
 };
 
 export default Board;
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,27 @@ import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
-      Card
+      <div className="card__content">
+        <div className="card__content-text">{props.text}</div>
+        <div className="card__content-emoji">{emoji.getUnicode(props.emoji || props.Emoji || '')}</div>
+        <button
+          onClick={() => props.deleteCardCallback(props.id)}
+          className="card__delete">
+          Delete
+        </button>
+      </div>
     </div>
   )
 }
 
 Card.propTypes = {
-
+  text: PropTypes.string.isRequired,
+  emoji: PropTypes.string,
+  Emoji: PropTypes.string,
+  deleteCardCallback: PropTypes.func.isRequired,
 };
 
 export default Card;
