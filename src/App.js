3b45a146e11bf0c84d3668f4fce4ac246e173137
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-01-18 23:58:59 -0800

    revised the App.js to call Search.js and to update the boardName for Board.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Board from './components/Board';
+import Search from './components/Search';
 
 const App = () => {
+  const [board, setBoard] = useState('{}')
+  const updateBoard = (item) => {
+    setBoard(item)
+  }
+
   return (
     <section>
       <header className="header">
         <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
+        <h1 className="header__h1"><span className="header__text">You are now at {board}'s Board</span></h1>
       </header>
+      <Search url="https://inspiration-board.herokuapp.com/boards" 
+              chooseBoardCallback={updateBoard} />
       <Board
         url="https://inspiration-board.herokuapp.com/"
-        board="boards" // for retrieve list of cards for a single board (not for delete a card)
-        card="cards"   // for delete a card
-        boardName={`Ting-Yi`}
+        board="boards" // for retrieving list of cards for a single board (not for delete a card)
+        card="cards"   // for deleting a card
+        boardName={board}
       />
     </section>
   );
